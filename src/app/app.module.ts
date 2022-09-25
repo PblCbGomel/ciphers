06622dcee2f4aa67a84638626f56076058e34d09
PblCbGomel <pblcb.gomel@gmail.com>
2022-09-25 23:35:32 +0300

    feat: add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { SettingsComponent } from './shared/components/header/settings/settings.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, SettingsComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
