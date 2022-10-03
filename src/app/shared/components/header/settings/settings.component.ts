42149965bec643e4e8c3019ca7a0d87aca074163
PblCbGomel <pblcb.gomel@gmail.com>
2022-10-03 03:14:54 +0300

    feat: add header and settings template

--- src/app/shared/components/header/settings/settings.component.ts
+++ src/app/shared/components/header/settings/settings.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  public items: MenuItem[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Language',
+        items: [
+          { label: 'Eng', icon: 'pi pi-sort-alt' },
+          { label: 'Ru', icon: 'pi pi-sort-alt' },
+        ],
+      },
+      {
+        label: 'Theme',
+        items: [
+          { label: 'Dark', icon: 'pi pi-sort-alt' },
+          { label: 'Light', icon: 'pi pi-sort-alt' },
+        ],
+      },
+    ];
   }
-
 }
--- src/app/shared/modules/main/main.module.ts
+++ src/app/shared/modules/main/main.module.ts
@@ -4,9 +4,24 @@ import { MainComponent } from '../../components/main/main.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { MainRoutingModule } from './main-routing.module';
+import { SettingsComponent } from '../../components/header/settings/settings.component';
+import { MenuModule } from 'primeng/menu';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { PanelMenuModule } from 'primeng/panelmenu';
 
 @NgModule({
-  declarations: [MainComponent, HeaderComponent, FooterComponent],
-  imports: [CommonModule, MainRoutingModule],
+  declarations: [
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    SettingsComponent,
+  ],
+  imports: [
+    CommonModule,
+    MainRoutingModule,
+    MenuModule,
+    TieredMenuModule,
+    PanelMenuModule,
+  ],
 })
 export class MainModule {}
