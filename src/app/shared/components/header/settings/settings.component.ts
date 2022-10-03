690357d8a0d6c743a1a25eeb88efb0f7b502a42c
PblCbGomel <pblcb.gomel@gmail.com>
2022-10-03 12:04:00 +0300

    feat: add settings menuitem

--- src/app/shared/components/header/settings/settings.component.ts
+++ src/app/shared/components/header/settings/settings.component.ts
@@ -13,17 +13,39 @@ export class SettingsComponent implements OnInit {
     this.items = [
       {
         label: 'Language',
+        icon: 'pi pi-fw pi-language',
         items: [
-          { label: 'Eng', icon: 'pi pi-sort-alt' },
-          { label: 'Ru', icon: 'pi pi-sort-alt' },
+          {
+            label: 'Eng',
+            icon: 'pi pi-fw pi-arrow-right-arrow-left',
+            command: (): void => {},
+          },
+          {
+            label: 'Ru',
+            icon: 'pi pi-fw pi-arrow-right-arrow-left',
+            command: (): void => {},
+          },
         ],
       },
       {
         label: 'Theme',
+        icon: 'pi pi-fw pi-envelope',
         items: [
-          { label: 'Dark', icon: 'pi pi-sort-alt' },
-          { label: 'Light', icon: 'pi pi-sort-alt' },
+          {
+            label: 'Light',
+            icon: 'pi pi-arrow-right-arrow-left',
+            command: (): void => {},
+          },
+          {
+            label: 'Dark',
+            icon: 'pi pi-arrow-right-arrow-left',
+            command: (): void => {},
+          },
         ],
+      },
+      {
+        label: 'Close',
+        icon: 'pi pi-fw pi-power-off',
       },
     ];
   }
--- src/app/shared/modules/main/main.module.ts
+++ src/app/shared/modules/main/main.module.ts
@@ -4,24 +4,10 @@ import { MainComponent } from '../../components/main/main.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { MainRoutingModule } from './main-routing.module';
-import { SettingsComponent } from '../../components/header/settings/settings.component';
-import { MenuModule } from 'primeng/menu';
-import { TieredMenuModule } from 'primeng/tieredmenu';
-import { PanelMenuModule } from 'primeng/panelmenu';
+import { HeaderModule } from '../../components/header/header.module';
 
 @NgModule({
-  declarations: [
-    MainComponent,
-    HeaderComponent,
-    FooterComponent,
-    SettingsComponent,
-  ],
-  imports: [
-    CommonModule,
-    MainRoutingModule,
-    MenuModule,
-    TieredMenuModule,
-    PanelMenuModule,
-  ],
+  declarations: [MainComponent, FooterComponent],
+  imports: [CommonModule, MainRoutingModule, HeaderModule],
 })
 export class MainModule {}
