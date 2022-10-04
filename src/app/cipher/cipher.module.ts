66c0b64e4272b208f935219e1114de26f194de7f
PblCbGomel <pblcb.gomel@gmail.com>
2022-10-04 20:37:14 +0300

    feat: add text input compinent

--- src/app/cipher/cipher.module.ts
+++ src/app/cipher/cipher.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CaesarCipherFormComponent } from './caesar-cipher-form/caesar-cipher-form.component';
 import { CipherRoutingModule } from './cipher-routing.module';
+import { InputComponent } from './caesar-cipher-form/input/input.component';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  declarations: [CaesarCipherFormComponent],
-  imports: [CommonModule, CipherRoutingModule],
+  declarations: [CaesarCipherFormComponent, InputComponent],
+  imports: [CommonModule, CipherRoutingModule, InputTextModule],
 })
 export class CihperModule {}
